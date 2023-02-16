dce70576969363df168ed51bd9479bb63820cded
alhamdulillah1234578 <kelvinrohmatsetiaji@gmail.com>
2023-02-16 09:13:51 +0700

    Coba

---
+++ src/CoreComponent.js
@@ -0,0 +1,30 @@
+import React from 'react';
+import {View, Text, Image, ScrollView, TextInput} from 'react-native';
+import Cat from './asset/Efes.jpg';
+
+const CoreComponent = () => {
+  return (
+    <ScrollView>
+      <Text>Some text</Text>
+      <View>
+        <Text>Some more text</Text>
+        <Image source={Cat} style={{width: 200, height: 200}} />
+        <Image source={Cat} style={{width: 200, height: 200}} />
+        <Image source={Cat} style={{width: 200, height: 200}} />
+        <Image source={Cat} style={{width: 200, height: 200}} />
+        <Image source={Cat} style={{width: 200, height: 200}} />
+        <Image source={Cat} style={{width: 200, height: 200}} />
+      </View>
+      <TextInput
+        style={{
+          height: 40,
+          borderColor: 'gray',
+          borderWidth: 1,
+        }}
+        defaultValue="You can type in me"
+      />
+    </ScrollView>
+  );
+};
+
+export default CoreComponent;
